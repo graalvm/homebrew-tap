ce7efc757946e0e904bcea672873e44c0b453801
Alexander Ronald Altman <alexanderaltman@me.com>
2020-05-15 15:42:13 -0700

    Add LTS release casks

---
+++ Casks/graalvm-ce-lts-java8.rb
@@ -0,0 +1,35 @@
+cask 'graalvm-ce-lts-java8' do
+  version '19.3.2'
+  sha256 '6db0e536893fcad314bbcec39dd598abd64f96e3dad9ffb9aa382ff18009f91e'
+
+  JVMS_DIR = '/Library/Java/JavaVirtualMachines'.freeze
+  TARGET_DIR = "#{JVMS_DIR}/#{cask}-#{version}".freeze
+
+  # github.com/graalvm/graalvm-ce-builds was verified as official when first introduced to the cask
+  url "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-#{version}/graalvm-ce-java8-darwin-amd64-#{version}.tar.gz"
+  appcast 'https://github.com/oracle/graal/releases.atom'
+  name 'GraalVM Community Edition, LTS (Java 8)'
+  homepage 'https://www.graalvm.org/'
+
+  artifact "graalvm-ce-java8-#{version}", target: TARGET_DIR
+
+  caveats <<~EOS
+    Installing GraalVM CE LTS (Java 8) in #{JVMS_DIR} requires root permissions.
+    You may be asked to enter your password to proceed.
+
+    On macOS Catalina, you may get a warning that "the developer cannot be
+    verified". This check can be disabled in the "Security & Privacy"
+    preferences pane or by running the following command:
+      xattr -r -d com.apple.quarantine #{TARGET_DIR}
+
+    To use GraalVM CE LTS, you may want to change your `JAVA_HOME`:
+      export JAVA_HOME=#{TARGET_DIR}/Contents/Home
+
+    or you may want to add its `bin` directory to your `PATH`:
+      export PATH=#{TARGET_DIR}/Contents/Home/bin:"$PATH"
+
+    GraalVM CE is licensed under the GPL 2 with Classpath exception:
+      https://github.com/oracle/graal/blob/master/LICENSE
+
+  EOS
+end
