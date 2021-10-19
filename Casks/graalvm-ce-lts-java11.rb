7f8729fcdab0c349287ee4d95f81fdc1b66c2aa0
Fabio Niephaus <fabio.niephaus@oracle.com>
2021-10-19 17:31:13 +0200

    Adapt to new style guidelines.

--- Casks/graalvm-ce-lts-java11.rb
+++ Casks/graalvm-ce-lts-java11.rb
@@ -1,44 +1,44 @@
-cask 'graalvm-ce-lts-java11' do
-  version '20.3.1'
-  sha256 '27a665b77191721e23134599038b6d6810767dd35d9ba27bc8f7ed2fbca57090'
+cask "graalvm-ce-lts-java11" do
+  version "20.3.1"
+  sha256 "27a665b77191721e23134599038b6d6810767dd35d9ba27bc8f7ed2fbca57090"
 
-  JVMS_DIR = '/Library/Java/JavaVirtualMachines'.freeze
-  TARGET_DIR = "#{JVMS_DIR}/#{cask}-#{version}".freeze
+  jvms_dir = "/Library/Java/JavaVirtualMachines".freeze
+  target_dir = "#{jvms_dir}/#{cask}-#{version}".freeze
 
   # github.com/graalvm/graalvm-ce-builds was verified as official when first introduced to the cask
   url "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-#{version}/graalvm-ce-java11-darwin-amd64-#{version}.tar.gz"
-  appcast 'https://github.com/oracle/graal/releases.atom'
-  name 'GraalVM Community Edition, LTS (Java 11)'
-  homepage 'https://www.graalvm.org/'
+  appcast "https://github.com/oracle/graal/releases.atom"
+  name "GraalVM Community Edition, LTS (Java 11)"
+  homepage "https://www.graalvm.org/"
 
-  artifact "graalvm-ce-java11-#{version}", target: TARGET_DIR
+  artifact "graalvm-ce-java11-#{version}", target: target_dir
 
   postflight do
     # Ensure GraalVM JDK 11 is listed by `/usr/libexec/java_home -V`.
-    macos_dir = "#{TARGET_DIR}/Contents/MacOS"
-    libjli_filename = 'libjli.dylib'
-    libjli_path = "#{TARGET_DIR}/Contents/Home/lib/jli/#{libjli_filename}"
+    macos_dir = "#{target_dir}/Contents/MacOS"
+    libjli_filename = "libjli.dylib"
+    libjli_path = "#{target_dir}/Contents/Home/lib/jli/#{libjli_filename}"
     libjli_symlink_path = "#{macos_dir}/#{libjli_filename}"
     next if File.exist?(libjli_symlink_path)
 
-    system_command '/bin/mkdir', args: ['-p', macos_dir], sudo: true
-    system_command '/bin/ln', args: ['-s', libjli_path, libjli_symlink_path], sudo: true
+    system_command "/bin/mkdir", args: ["-p", macos_dir], sudo: true
+    system_command "/bin/ln", args: ["-s", libjli_path, libjli_symlink_path], sudo: true
   end
 
   caveats <<~EOS
-    Installing GraalVM CE LTS (Java 11) in #{JVMS_DIR} requires root permissions.
+    Installing GraalVM CE LTS (Java 11) in #{jvms_dir} requires root permissions.
     You may be asked to enter your password to proceed.
 
     On macOS Catalina, you may get a warning that "the developer cannot be
     verified". This check can be disabled in the "Security & Privacy"
     preferences pane or by running the following command:
-      xattr -r -d com.apple.quarantine #{TARGET_DIR}
+      xattr -r -d com.apple.quarantine #{target_dir}
 
     To use GraalVM CE LTS, you may want to change your `JAVA_HOME`:
-      export JAVA_HOME=#{TARGET_DIR}/Contents/Home
+      export JAVA_HOME=#{target_dir}/Contents/Home
 
     or you may want to add its `bin` directory to your `PATH`:
-      export PATH=#{TARGET_DIR}/Contents/Home/bin:"$PATH"
+      export PATH=#{target_dir}/Contents/Home/bin:"$PATH"
 
     GraalVM CE is licensed under the GPL 2 with Classpath exception:
       https://github.com/oracle/graal/blob/master/LICENSE
