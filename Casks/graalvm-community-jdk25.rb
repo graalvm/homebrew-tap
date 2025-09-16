b4fcf1efe1d363d97558217b7db0c7f9d25c65fe
Yassine Hadry <yassine.hadry@oracle.com>
2025-09-16 16:28:11 +0100

    Introduce GraalVM 25 (#67)

---
+++ Casks/graalvm-community-jdk25.rb
@@ -0,0 +1,43 @@
+cask "graalvm-community-jdk25" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "25"
+  sha256 arm:   "c446d5aaeda98660a4c14049d299e9fba72105a007df89f19d27cf3979d37158",
+         intel: "04278cf867d040e29dc71dd7727793f0ea67eb72adce8a35d04b87b57906778d"
+
+  installation_dir = "graalvm-community-openjdk-#{version}+37.1".freeze
+  jvms_dir = "/Library/Java/JavaVirtualMachines".freeze
+  target_dir = "#{jvms_dir}/graalvm-community-openjdk-#{version.split(".").first}".freeze
+
+  # github.com/graalvm/graalvm-ce-builds was verified as official when first introduced to the cask
+  url "https://github.com/graalvm/graalvm-ce-builds/releases/download/jdk-#{version}.0.0/graalvm-community-jdk-#{version}.0.0_macos-#{arch}_bin.tar.gz"
+  name "GraalVM Community Edition 25"
+  homepage "https://www.graalvm.org/"
+
+  artifact installation_dir, target: target_dir
+
+  postflight do
+    # Correct symlink
+    macos_dir = "#{target_dir}/Contents/MacOS"
+    libjli_filename = "libjli.dylib"
+    libjli_path = "#{target_dir}/Contents/Home/lib/#{libjli_filename}"
+    libjli_symlink_path = "#{macos_dir}/#{libjli_filename}"
+    system_command "/bin/mkdir", args: ["-p", macos_dir], sudo: true
+    system_command "/bin/ln", args: ["-s", "-f", libjli_path, libjli_symlink_path], sudo: true
+  end
+
+  caveats <<~EOS
+    Installing GraalVM CE 25 in #{jvms_dir} requires root permission.
+    You may be asked to enter your password to proceed.
+
+    To use GraalVM, you may want to change your $JAVA_HOME:
+      export JAVA_HOME="#{target_dir}/Contents/Home"
+
+    or you may want to add its `bin` directory to your $PATH:
+      export PATH="#{target_dir}/Contents/Home/bin:$PATH"
+
+    GraalVM CE is licensed under the GPL 2 with Classpath exception:
+      https://github.com/oracle/graal/blob/master/LICENSE
+
+  EOS
+end
