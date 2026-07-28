c0a857bd4d21f8f5f8a1bb5c621559e046139b45
yassine_hadry <yassine.hadry@oracle.com>
2026-07-28 12:48:08 +0100

    Update JDK 25i2 casks with feature release

--- Casks/graalvm-jdk25i.rb
+++ Casks/graalvm-jdk25i.rb
@@ -1,15 +1,15 @@
 cask "graalvm-jdk25i" do
   arch arm: "aarch64"
 
-  version "25.1.3"
-  sha256 arm: "3b694d60985503bf561cd2444fb1a9b697b08fb700d0bb27d12c49adc5d914bb"
+  version "25.2.4"
+  sha256 arm: "1b5937aa3076707459cfc815a1699761f943d2d1c9cbe03388e36d5e47eb27c3"
 
-  installation_dir = "graalvm-#{version}+9.1".freeze
+  installation_dir = "graalvm-#{version}+7.1".freeze
   jvms_dir = "/Library/Java/JavaVirtualMachines".freeze
   target_dir = "#{jvms_dir}/graalvm-jdk-#{version.split(".").first}".freeze
 
   # download.oracle.com was verified as official when first introduced to the cask
-  url "https://download.oracle.com/graalvm/25i1/archive/graalvm-jdk-25i1-25.0.3_macos-#{arch}_bin.tar.gz"
+  url "https://gds.oracle.com/download/graal/25i2/archive/graalvm-jdk-25i2-25.0.4_macos-#{arch}_bin.tar.gz"
   name "Oracle GraalVM 25i"
   homepage "https://www.graalvm.org/"
 
